cf83e96d5378ec9c45ab9ed649bcd2b525a97ae9
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-21 21:56:51 +0300

    BaseTest: Add default `$phony` instance and set up on `setUp()` method

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -2,13 +2,23 @@
 
 namespace Deligoez\Phony\Tests;
 
+use Deligoez\Phony\Phony;
 use Orchestra\Testbench\TestCase;
 use Deligoez\Phony\PhonyServiceProvider;
 
 abstract class BaseTest extends TestCase
 {
+    protected Phony $phony;
+
     protected function getPackageProviders($app): array
     {
         return [PhonyServiceProvider::class];
+    }
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->phony = app('phony');
     }
 }
