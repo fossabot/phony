d9766c2a1b7002025690ad25e22b18f2357af37d
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-22 15:18:20 +0300

    BaseTest: Remove `$tester` object

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -3,14 +3,12 @@
 namespace Deligoez\Phony\Tests;
 
 use Deligoez\Phony\Phony;
-use Deligoez\Phony\Fakes\Fake;
 use Orchestra\Testbench\TestCase;
 use Deligoez\Phony\PhonyServiceProvider;
 
 abstract class BaseTest extends TestCase
 {
     protected Phony $phony;
-    protected Fake $tester;
 
     protected function getPackageProviders($app): array
     {
@@ -22,10 +20,5 @@ abstract class BaseTest extends TestCase
         parent::setUp();
 
         $this->phony = app('phony');
-    }
-
-    protected function setTester(Fake $fake): void
-    {
-        $this->tester = $fake;
     }
 }
