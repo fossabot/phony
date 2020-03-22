0e49e01325d409430d7e89ddaa21f2dac753f9a5
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-22 15:08:25 +0300

    BaseTest: Introduce `$tester` object

--- tests/BaseTest.php
+++ tests/BaseTest.php
@@ -3,12 +3,14 @@
 namespace Deligoez\Phony\Tests;
 
 use Deligoez\Phony\Phony;
+use Deligoez\Phony\Fakes\Fake;
 use Orchestra\Testbench\TestCase;
 use Deligoez\Phony\PhonyServiceProvider;
 
 abstract class BaseTest extends TestCase
 {
     protected Phony $phony;
+    protected Fake $tester;
 
     protected function getPackageProviders($app): array
     {
@@ -20,5 +22,10 @@ abstract class BaseTest extends TestCase
         parent::setUp();
 
         $this->phony = app('phony');
+    }
+
+    protected function setTester(Fake $fake): void
+    {
+        $this->tester = $fake;
     }
 }
