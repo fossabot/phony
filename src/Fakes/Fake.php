94b9c30a3d89c9fb6c706172ad0212c8d5dc6515
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-23 17:36:37 +0300

    Fake: Rewrite `fetchOne()` method

--- src/Fakes/Fake.php
+++ src/Fakes/Fake.php
@@ -26,8 +26,36 @@ class Fake
      */
     public function fetchOne(string $key, array $replace = []): string
     {
-        return Arr::random(
+        // Retrieve element by the given $key
+        $template =  Arr::random(
             trans("phony::{$key}", $replace, $this->phony->defaultLocale)
         );
+
+        // Check if it's an actual fake data or a template
+        if (!Str::contains($template, ':'))
+        {
+            // It's a fake data, so return it immediately.
+            return $template;
+        }
+
+        // Extract placeholders in the template
+        $placeholders = array_map(
+            'trim',
+            array_filter(explode(':', $template))
+        );
+
+        $values = [];
+
+        // Fetch placeholder values recursively
+        foreach ($placeholders as $placeholder) {
+            $values[$placeholder] = $this->fetchOne($placeholder, $replace);
+        }
+
+        // Replace placeholders by their values
+        foreach ($values as $name => $value) {
+            $template = Str::of($template)->replaceFirst(':'.$name, $value);
+        }
+
+        return $template;
     }
 }
