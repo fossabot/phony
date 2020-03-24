a6e8fb5c24d3105daac018fe2fcfa33bc0642958
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-24 17:26:49 +0300

    PersonTrTest: Fix

--- tests/Locales/Tr/PersonTrTest.php
+++ tests/Locales/Tr/PersonTrTest.php
@@ -7,15 +7,14 @@ class PersonTrTest extends BaseTrTest
     /** @test */
     public function initials(): void
     {
-        $this->markTestSkipped();
-
         $this->assertEquals(
-            3,
+            6,
             mb_strlen($this->🙃->person()->initials(), 'utf8')
         );
 
+        $times = random_int(2, 10);
         $this->assertEquals(
-            $times = random_int(2, 10),
+            $times * 2,
             mb_strlen($this->🙃->person()->initials($times), 'utf8')
         );
     }
