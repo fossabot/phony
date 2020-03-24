f8186e94e4d577cbf25857b05ac7fa17f337414f
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-24 18:49:38 +0300

    Add draft Address Fake

--- src/Fakes/Standard/Address.php
+++ src/Fakes/Standard/Address.php
@@ -9,14 +9,36 @@ class Address extends Fake
     /**
      * Produces the name of a city.
      *
-     * @param  bool  $withState
+     * @param  bool    $withState
+     * @param  int     $times
+     * @param  bool    $asString
+     * @param  string  $glue
      *
-     * @return string
+     * @return array|string
      *
-     * @example Phony::address()->city() #=> "Zeus"
+     * @example 🙃::address()->city() #=> "Imogeneborough"
+     * @example 🙃::address()->city(true) #=> "Northfort, California"
      */
-    public function city(bool $withState): string
+    public function city(bool $withState = false, int $times = 1, bool $asString = false, string $glue = ' ')
     {
-        return $this->fetchOne('address.city');
+        return $withState
+            ? $this->fetch('address.city', $times, $asString, $glue)
+            : $this->fetch('address.city_with_state', $times, $asString, $glue);
+    }
+
+    /**
+     * Produces the name of a city.
+     *
+     * @param  int     $times
+     * @param  bool    $asString
+     * @param  string  $glue
+     *
+     * @return array|string
+     *
+     * @example 🙃::address()->streetName() #=> "Larkin Fork"
+     */
+    public function streetName(int $times = 1, bool $asString = false, string $glue = ' ')
+    {
+        return $this->fetch('address.street_name', $times, $asString, $glue);
     }
 }
--- tests/Standard/AddressTest.php
+++ tests/Standard/AddressTest.php
@@ -6,10 +6,27 @@ use Deligoez\Phony\Tests\BaseTest;
 
 class AddressTest extends BaseTest
 {
-    public function test_city(): void
+    /** @test */
+    public function city(): void
     {
         $this->assertIsString(
-            $this->phony->address()->city()
+            $this->🙃->address()->city()
+        );
+    }
+
+    /** @test */
+    public function city_with_state(): void
+    {
+        $this->assertIsString(
+            $this->🙃->address()->city(true)
+        );
+    }
+
+    /** @test */
+    public function street_name(): void
+    {
+        $this->assertIsString(
+            $this->🙃->address()->streetName()
         );
     }
 }
