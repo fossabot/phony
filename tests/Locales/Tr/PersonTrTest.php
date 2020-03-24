72173b0641517ed54caa721a164ac6fb049e2072
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-24 18:35:37 +0300

    Update PersonTrTest.php

--- tests/Locales/Tr/PersonTrTest.php
+++ tests/Locales/Tr/PersonTrTest.php
@@ -5,14 +5,19 @@ namespace Deligoez\Phony\Tests\Locales\Tr;
 class PersonTrTest extends BaseTrTest
 {
     /** @test */
-    public function initials(): void
+    public function initials_with_default_lenght(): void
     {
         $this->assertEquals(
             6,
             mb_strlen($this->🙃->person()->initials(), 'utf8')
         );
+    }
 
+    /** @test */
+    public function initials_with_given_lenght(): void
+    {
         $times = random_int(2, 10);
+
         $this->assertEquals(
             $times * 2,
             mb_strlen($this->🙃->person()->initials($times), 'utf8')
