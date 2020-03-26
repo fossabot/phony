5370a05371c90c3a5bfc3f2f165bde7bb3b2a2ec
Yunus Emre Deligöz <ye@deligoz.me>
2020-03-26 21:39:51 +0300

    Update AddressTest.php

--- tests/Standard/AddressTest.php
+++ tests/Standard/AddressTest.php
@@ -17,169 +17,144 @@ class AddressTest extends BaseTest
     /** @test */
     public function can_call_by_emoji_alias(): void
     {
+        // TODO: Move this test to Phony class
         $this->assertInstanceOf(
             Fake::class,
             $this->🙃->📫()
         );
     }
 
-    /** @test */
-    public function city(): void
-    {
-        $this->assertIsString(
-            $this->🧪->city()
-        );
-    }
+    // region Magic Attributes
 
     /** @test */
-    public function city_with_state(): void
+    public function cityAttribute(): void
     {
         $this->assertIsString(
-            $this->🧪->city(true)
+            $this->🧪->city
         );
     }
 
     /** @test */
-    public function street_name(): void
+    public function cityWithStateAttribute(): void
     {
         $this->assertIsString(
-            $this->🧪->streetName()
-        );
-    }
-
-    /** @test
-     * @throws \Exception
-     */
-    public function secondary_address(): void
-    {
-        $this->assertIsString(
-            $this->🧪->secondaryAddress()
-        );
-    }
-
-    /** @test
-     * @throws \Exception
-     */
-    public function street_address(): void
-    {
-        $this->assertIsString(
-            $this->🧪->streetAddress()
-        );
-    }
-
-    /** @test
-     * @throws \Exception
-     */
-    public function street_address_secondary_address_included(): void
-    {
-        $this->assertIsString(
-            $this->🧪->streetAddress(true)
-        );
-    }
-
-    /** @test
-     * @throws \Exception
-     */
-    public function building_number(): void
-    {
-        $this->assertIsString(
-            $this->🧪->buildingNumber()
+            $this->🧪->cityWithState
         );
     }
 
     /** @test */
-    public function community(): void
+    public function streetNameAttribute(): void
     {
         $this->assertIsString(
-            $this->🧪->community()
+            $this->🧪->streetName
         );
     }
 
-    /** @test
-     * @throws \Exception
-     */
-    public function mailBox(): void
+    /** @test */
+    public function secondaryAddressAttribute(): void
     {
         $this->assertIsString(
-            $this->🧪->mailBox()
+            $this->🧪->secondaryAddress
         );
     }
 
-    /** @test
+    /** @test */
+    public function streetAddressAttribute(): void
+    {
+        $this->assertIsString(
+            $this->🧪->streetAddress
+        );
+    }
+
+    /** @test */
+    public function streetAddressWithSecondaryAddressAttribute(): void
+    {
+        $this->assertIsString(
+            $this->🧪->streetAddressWithSecondaryAddress
+        );
+    }
+
+    /** @test */
+    public function buildingNumberAttribute(): void
+    {
+        $this->assertIsString(
+            $this->🧪->buildingNumber
+        );
+    }
+
+    /** @test */
+    public function communityAttribute(): void
+    {
+        $this->assertIsString(
+            $this->🧪->community
+        );
+    }
+
+    /** @test */
+    public function mailBoxAttribute(): void
+    {
+        $this->assertIsString(
+            $this->🧪->mailBox
+        );
+    }
+
+    /** @test */
+    public function zipCodeAttribute(): void
+    {
+        $this->assertIsString(
+            $this->🧪->zipCode
+        );
+    }
+
+    /** @test */
+    public function streetSuffixAttribute(): void
+    {
+        $this->assertIsString(
+            $this->🧪->streetSuffix
+        );
+    }
+
+    /** @test */
+    public function citySuffixAttribute(): void
+    {
+        $this->assertIsString(
+            $this->🧪->citySuffix
+        );
+    }
+
+    /** @test */
+    public function cityPrefixAttribute(): void
+    {
+        $this->assertIsString(
+            $this->🧪->cityPrefix
+        );
+    }
+
+    // endregion
+
+    // region Functions
+
+    /** @test *
+     *
      * @throws \Exception
      */
-    public function zip_code(): void
+    public function zipCode(): void
     {
         $this->assertIsString(
             $this->🧪->zipCode()
         );
-
-        $this->assertIsString(
-            $this->🧪->zip()
-        );
-
-        $this->assertIsString(
-            $this->🧪->postcode()
-        );
     }
 
     /** @test
+     *
      * @throws \Exception
      */
-    public function zip_code_for_state_abbreviation(): void
+    public function zipCodeWithStateAbbreviation(): void
     {
         $this->assertIsString(
             $this->🧪->zipCode('CO')
         );
-
-        $this->assertIsString(
-            $this->🧪->zip('CO')
-        );
-
-        $this->assertIsString(
-            $this->🧪->postcode('CO')
-        );
     }
 
-    /** @test */
-    public function street_suffix(): void
-    {
-        $this->assertIsString(
-            $this->🧪->streetSuffix()
-        );
-    }
-
-    /** @test */
-    public function city_suffix(): void
-    {
-        $this->assertIsString(
-            $this->🧪->citySuffix()
-        );
-    }
-
-    /** @test */
-    public function city_prefix(): void
-    {
-        $this->assertIsString(
-            $this->🧪->cityPrefix()
-        );
-    }
-
-    /** @test */
-    public function can_access_by_magic_attributes(): void
-    {
-        $this->assertNotNull($this->🧪->city);
-        $this->assertNotNull($this->🧪->streetName);
-        $this->assertNotNull($this->🧪->secondaryAddress);
-        $this->assertNotNull($this->🧪->streetAddress);
-        $this->assertNotNull($this->🧪->buildingNumber);
-        $this->assertNotNull($this->🧪->community);
-        $this->assertNotNull($this->🧪->mailBox);
-        $this->assertNotNull($this->🧪->zipCode);
-        $this->assertNotNull($this->🧪->zip);
-        $this->assertNotNull($this->🧪->postcode);
-        $this->assertNotNull($this->🧪->streetSuffix);
-        $this->assertNotNull($this->🧪->citySuffix);
-        $this->assertNotNull($this->🧪->cityPrefix);
-    }
+    // endregion
 }
