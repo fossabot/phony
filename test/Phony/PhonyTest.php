48ab2faf74ccffad839e581e7f64aa0c6a755482
Yunus Emre Deligöz <ye@deligoz.me>
2020-04-17 08:41:19 +0300

    :white_check_mark: Divide PhonyTest.php into multiple meaningful test files

--- test/Phony/PhonyTest.php
+++ test/Phony/PhonyTest.php
@@ -4,38 +4,9 @@ namespace Phony\Test\Phony;
 
 use Phony\Fake\Fake;
 use Phony\Test\BaseTest;
-use RuntimeException;
 
 class PhonyTest extends BaseTest
 {
-    /** @test */
-    public function can_not_access_undefined_magic_attribute(): void
-    {
-        $this->expectException(RuntimeException::class);
-
-        $this->🙃->not_exist;
-    }
-
-    /** @test */
-    public function can_not_set_a_magic_attribute(): void
-    {
-        $this->expectException(RuntimeException::class);
-
-        $this->🙃->alphabet = 'can-not';
-    }
-
-    /** @test */
-    public function can_check_existence_with_magic_isset(): void
-    {
-        $this->assertTrue(
-            isset($this->🙃->alphabet)
-        );
-
-        $this->assertFalse(
-            isset($this->🙃->not_exist)
-        );
-    }
-
     /** @test */
     public function can_call_by_an_alias(): void
     {
