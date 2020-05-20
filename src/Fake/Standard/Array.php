6a3ccc22a28b005e896a7d41d1e7ad777630a8f2
Yunus Emre Deligöz <ye@deligoz.me>
2020-05-20 15:40:34 +0300

    Add draft Array Fake

---
+++ src/Fake/Standard/Array.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Phonyland\Fake\Standard;
+
+use Phonyland\Fake\Fake;
+
+/**
+ * Class Number.
+ */
+class Arrays extends Fake
+{
+    // TODO: randomElement
+    // TODO: randomElements
+    // TODO: randomKey
+    // TODO: randomKeys
+}
